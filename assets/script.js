695a767948bba6f5519b4b58f21d3c0f37652605
farrelpratama <farrepratama512@gmail.com>
2024-08-17 20:36:34 +0700

    show picture update

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,21 @@ hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("open");
   navActive.classList.toggle("active");
 });
+
+const galery = document.querySelector(".galery .container");
+const overlay = document.querySelector(".galery .overlay");
+
+galery.addEventListener("click", function (e) {
+  if (e.target.className == "image") {
+    overlay.classList.add("active");
+
+    const imagePrev = document.querySelector(".galery .overlay .preview img");
+    imagePrev.src = e.target.src;
+  }
+});
+
+overlay.addEventListener("click", function (e) {
+  if (e.target.className == "close") {
+    overlay.classList.remove("active");
+  }
+});
